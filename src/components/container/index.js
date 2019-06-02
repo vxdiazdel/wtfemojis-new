d1274aabae4c6616aa59ac782937911b70b3a69e
Victor Diaz de Leon <vdiazdeleon@weedmaps.com>
2019-06-02 15:11:25 -0700

    Add Container component

---
+++ src/components/container/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import * as Styled from "./styled-components";
+
+const Container = ({ children }) => (
+  <Styled.Container>{children}</Styled.Container>
+);
+
+export default Container;
