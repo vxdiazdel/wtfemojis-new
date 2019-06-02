befaaa6c16b65672445f26df4cec93968e50c1b3
Victor Diaz de Leon <vdiazdeleon@weedmaps.com>
2019-06-02 15:08:05 -0700

    Add Nav component

---
+++ src/components/nav/index.js
@@ -0,0 +1,20 @@
+import React from "react";
+import SearchBar from "../search-bar";
+import * as Styled from "./styled-components";
+
+const Nav = () => (
+  <Styled.Navbar>
+    <Styled.NavContainer>
+      <Styled.NavbarLeft>
+        <a href="/">
+          wtfEmojis<span role="img" aria-label="OK hand">👌</span>
+        </a>
+      </Styled.NavbarLeft>
+      <Styled.NavbarRight>
+        <SearchBar />
+      </Styled.NavbarRight>
+    </Styled.NavContainer>
+  </Styled.Navbar>
+);
+
+export default Nav;
---
+++ src/components/nav/styled-components.js
@@ -0,0 +1,29 @@
+import styled from "styled-components";
+import { rem } from "polished";
+import { colors } from "../../styles";
+
+export const Navbar = styled.nav`
+  height: 60px;
+  background: ${colors.periwinkle};
+  padding: 0 ${rem(16)};
+`;
+
+export const NavContainer = styled.div`
+  height: inherit;
+  margin: 0 auto;
+  max-width: 1200px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+export const NavbarLeft = styled.div`
+  a {
+    color: white;
+    font-size: ${rem(24)};
+  }
+`;
+
+export const NavbarRight = styled.div`
+
+`;
