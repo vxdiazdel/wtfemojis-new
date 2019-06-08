03d4fb65e1e856bfa2642d894369dd681ecb1fe4
Victor Diaz de Leon <vrdiazdel@gmail.com>
2019-06-08 12:43:48 -0700

    Add copyToClipboard functionality

--- src/components/emoji/index.js
+++ src/components/emoji/index.js
@@ -1,12 +1,53 @@
-import React from "react";
+import React, { useContext } from "react";
+import { ErrorContext } from "../../stores/error-context";
 import * as Styled from "./styled-components";
 
-const Emoji = ({ data: { name, emoji } }) => (
-  <Styled.EmojiWrap title={name}>
-    <Styled.Emoji role="img" aria-label={name}>
-      {emoji}
-    </Styled.Emoji>
-  </Styled.EmojiWrap>
-);
+const Emoji = ({ data: { name, emoji, _id } }) => {
+  const [, setError] = useContext(ErrorContext);
+
+  const copyToClipboard = async el => {
+    setError(null);
+
+    if (navigator.clipboard) {
+      await navigator.clipboard.writeText(el);
+      setError(false);
+
+      return setTimeout(() => {
+        setError(null);
+      }, 1250);
+    }
+
+    const emoji = document.getElementById(_id).firstChild;
+    const range = document.createRange();
+    const selection = window.getSelection();
+
+    range.selectNodeContents(emoji);
+    selection.removeAllRanges();
+    selection.addRange(range);
+
+    if (document.execCommand("copy")) {
+      selection.removeAllRanges();
+      setError(false);
+    } else {
+      setError(true);
+    }
+
+    return setTimeout(() => {
+      setError(null);
+    }, 1250);
+  };
+
+  return (
+    <Styled.EmojiWrap
+      title={name}
+      id={_id}
+      onClick={() => copyToClipboard(emoji)}
+    >
+      <Styled.Emoji role="img" aria-label={name}>
+        {emoji}
+      </Styled.Emoji>
+    </Styled.EmojiWrap>
+  );
+};
 
 export default Emoji;
