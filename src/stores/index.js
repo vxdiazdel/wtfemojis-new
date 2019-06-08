a4b22e716f486781ceb47e2f9f52fbc879102f89
Victor Diaz de Leon <vrdiazdel@gmail.com>
2019-06-08 12:42:01 -0700

    Add Error store

--- src/stores/index.js
+++ src/stores/index.js
@@ -1,16 +1,20 @@
 import React, { useState } from "react";
 import { EmojiContext } from "./emoji-context";
 import { SearchContext } from "./search-context";
+import { ErrorContext } from "./error-context";
 import emojiData from "../config/emojis";
 
 const Store = ({ children }) => {
   const [searchText, setSearchText] = useState("");
   const [emojis, setEmojis] = useState({ all: emojiData, filtered: emojiData });
+  const [hasError, setError] = useState(null);
 
   return (
     <SearchContext.Provider value={[searchText, setSearchText]}>
       <EmojiContext.Provider value={[emojis, setEmojis]}>
-        {children}
+        <ErrorContext.Provider value={[hasError, setError]}>
+          {children}
+        </ErrorContext.Provider>
       </EmojiContext.Provider>
     </SearchContext.Provider>
   );
