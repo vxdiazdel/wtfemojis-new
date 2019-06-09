84e61b5fb1c6560da1e8b554d51902964b5ad41a
Victor Diaz de Leon <vrdiazdel@gmail.com>
2019-06-08 18:06:03 -0700

    Update Container component

--- src/components/container/index.js
+++ src/components/container/index.js
@@ -1,8 +1,10 @@
 import React from "react";
 import * as Styled from "./styled-components";
 
-const Container = ({ children }) => (
-  <Styled.Container>{children}</Styled.Container>
+const Container = ({ maxWidth, children }) => (
+  <Styled.Container maxWidth={maxWidth}>
+    {children}
+  </Styled.Container>
 );
 
 export default Container;
