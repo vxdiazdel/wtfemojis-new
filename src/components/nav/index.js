d8e2c2e447d6d8761cc1de6fad1ba5b712004454
Victor Diaz de Leon <vrdiazdel@gmail.com>
2019-06-08 17:55:47 -0700

    Add scrollToTop

--- src/components/nav/index.js
+++ src/components/nav/index.js
@@ -2,20 +2,31 @@ import React from "react";
 import SearchBar from "../search-bar";
 import * as Styled from "./styled-components";
 
-const Nav = () => (
-  <Styled.Navbar>
-    <Styled.NavContainer>
-      <Styled.NavbarLeft>
-        <a href="/">
-          wtfEmojis
-          <span role="img" aria-label="OK hand">
-            👌
-          </span>
-        </a>
-      </Styled.NavbarLeft>
-      <SearchBar />
-    </Styled.NavContainer>
-  </Styled.Navbar>
-);
+const Nav = () => {
+  const scrollToTop = e => {
+    e.preventDefault();
+    window.scrollTo({
+      top: 0,
+      left: 0,
+      behavior: "smooth"
+    });
+  };
+
+  return (
+    <Styled.Navbar>
+      <Styled.NavContainer>
+        <Styled.NavbarLeft onClick={scrollToTop}>
+          <a href="/">
+            wtfEmojis
+            <span role="img" aria-label="OK hand">
+              👌
+            </span>
+          </a>
+        </Styled.NavbarLeft>
+        <SearchBar />
+      </Styled.NavContainer>
+    </Styled.Navbar>
+  );
+};
 
 export default Nav;
