4ab25f940104cbbdc8b01240369a8bb77bd74714
Victor Diaz de Leon <vrdiazdel@gmail.com>
2019-06-16 17:42:15 -0700

    Add Loading context

--- src/components/emoji-list/index.js
+++ src/components/emoji-list/index.js
@@ -1,12 +1,17 @@
 import React, { useContext } from "react";
 import { EmojiContext } from "../../stores/emoji-context";
+import { LoadingContext } from "../../stores/loading-context";
 import Emoji from "../emoji";
 import NotFound from "../not-found";
+import Loader from "../loader";
 import * as Styled from "./styled-components";
 
 const EmojiList = () => {
   const [emojis] = useContext(EmojiContext);
+  const [isLoading] = useContext(LoadingContext);
   const { filtered } = emojis;
+
+  if (isLoading) return <Loader />;
 
   return (
     <Styled.ListWrap>
--- src/stores/index.js
+++ src/stores/index.js
@@ -1,22 +1,42 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { EmojiContext } from "./emoji-context";
 import { SearchContext } from "./search-context";
 import { ErrorContext } from "./error-context";
-import emojiData from "../config/emojis";
+import { LoadingContext } from "./loading-context";
 
 const Store = ({ children }) => {
   const [searchText, setSearchText] = useState("");
-  const [emojis, setEmojis] = useState({ all: emojiData, filtered: emojiData });
+  const [emojis, setEmojis] = useState({ all: [], filtered: [] });
   const [hasError, setError] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    setIsLoading(true);
+    if (!localStorage.getItem('emojiData')) {
+      fetch('https://raw.githubusercontent.com/vxdiazdel/wtfemojis/master/config/emojis.json')
+        .then(res => res.json())
+        .then(data => {
+          setEmojis({ all: data, filtered: data });
+          setIsLoading(false);
+          localStorage.setItem('emojiData', JSON.stringify(data));
+        });
+    } else {
+      const data = JSON.parse(localStorage.getItem('emojiData'));
+      setEmojis({ all: data, filtered: data });
+      setIsLoading(false);
+    }
+  }, []);
 
   return (
-    <SearchContext.Provider value={[searchText, setSearchText]}>
-      <EmojiContext.Provider value={[emojis, setEmojis]}>
-        <ErrorContext.Provider value={[hasError, setError]}>
-          {children}
-        </ErrorContext.Provider>
-      </EmojiContext.Provider>
-    </SearchContext.Provider>
+    <LoadingContext.Provider value={[isLoading, setIsLoading]}>
+      <SearchContext.Provider value={[searchText, setSearchText]}>
+        <EmojiContext.Provider value={[emojis, setEmojis]}>
+          <ErrorContext.Provider value={[hasError, setError]}>
+            {children}
+          </ErrorContext.Provider>
+        </EmojiContext.Provider>
+      </SearchContext.Provider>
+    </LoadingContext.Provider>
   );
 };
 
