84542b1eba40a77e764544b79d02aa8a130be5ac
Victor Diaz de Leon <vrdiazdel@gmail.com>
2019-07-27 12:59:12 -0700

    Update search filter

--- src/components/search-bar/index.js
+++ src/components/search-bar/index.js
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect } from "react";
+import React, { useContext } from "react";
 import { SearchContext } from "../../stores/search-context";
 import { EmojiContext } from "../../stores/emoji-context";
 import * as Styled from "./styled-components";
@@ -8,21 +8,24 @@ const SearchBar = () => {
   const [emojis, setEmojis] = useContext(EmojiContext);
   const { all } = emojis;
 
-  useEffect(() => {
-    if (!searchText.length) return setEmojis({ all, filtered: all });
+  const handleChange = e => {
+    const { value } = e.target;
+    setSearchText(value);
 
+    if (!value) return setEmojis({ all, filtered: all });
+    
     const filtered = all.filter(
-      x => x.name.toLowerCase().indexOf(searchText) !== -1
+      x => x.name.toLowerCase().indexOf(value) !== -1
     );
 
     setEmojis({ all, filtered });
-  }, [all, searchText, setEmojis]);
+  }
 
   return (
     <Styled.Input
       type="text"
       placeholder="Search..."
-      onChange={e => setSearchText(e.target.value)}
+      onChange={handleChange}
       value={searchText}
     />
   );
