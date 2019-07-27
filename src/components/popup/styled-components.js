2eb9c04a13804f8061a8e0e3d1c34faad3d5f64f
Victor Diaz de Leon <vrdiazdel@gmail.com>
2019-07-27 14:01:23 -0700

    Style updates

--- src/components/popup/styled-components.js
+++ src/components/popup/styled-components.js
@@ -7,6 +7,10 @@ const { white, teal, pink } = colors;
 export const Popup = styled.div`
   background: ${({ hasError }) => (hasError ? pink : teal)};
   border-radius: ${rem(6)};
+  box-shadow: 
+    0 2px 2px 0 rgba(0,0,0,0.14), 
+    0 3px 1px -2px rgba(0,0,0,0.12), 
+    0 1px 5px 0 rgba(0,0,0,0.2);
   color: ${white};
   font-size: ${rem(18)};
   opacity: ${({ hasError }) => (hasError === null ? 0 : 1)};
